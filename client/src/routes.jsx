17c5c29eaa2ee85ed877b488e8350f3fce387b9f
juniorHotes <juniorhotes53@gmail.com>
2020-11-27 17:54:20 -0300

    New routes.

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Home from './pages/Home/Home';
-// import Question from './pages/Question';
+import NewQuestion from './pages/Question/NewQuestion';
 import Respond from './pages/Respond/Respond';
 
 function Routes() {
   return (
     <BrowserRouter>
       <Route path="/" exact component={Home} />
+      <Route path="/search" component={Home} />
+      <Route path="/newquestion" component={NewQuestion} />
       <Route path="/question" component={Respond} />
-      {/* <Route path="/savequestion" component={Question} />
-      <Route path="/question/response" component={Respond} /> */}
     </BrowserRouter>
   );
 }
