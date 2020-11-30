33370c095c818ffdf6c6f8612f8745c29dbc12f9
juniorHotes <juniorhotes53@gmail.com>
2020-11-30 12:59:21 -0300

    Adding a search form.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Navbar() {
+    const [getQuery, setQuery] = useState('')
+
     return (
         <div className="navbar navbar-light bg-primary">
             <Link className="navbar-brand" to="/">
                 <h3>Myblog</h3>
             </Link>
+            <form>
+                <input type="search"
+                    name="search"
+                    value={getQuery}
+                    onChange={(e) => setQuery(e.target.value)}
+                />
+                <button type="submit">Search</button>
+            </form>
         </div>
     )
 }
